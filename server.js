d9163e719fabb359d9f8f24c75172f3ddf8913ee
crystalodi <crystalodi@gmail.com>
2018-06-23 00:11:39 -0500

    add model for article and route to serve static page.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const routes = require("./routes");
 const app = express();
 
 app.use(bodyParser.urlencoded({extended:true}));
@@ -12,11 +13,9 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Send every request to the React app
-// Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+app.use(routes);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/nytreact");
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
