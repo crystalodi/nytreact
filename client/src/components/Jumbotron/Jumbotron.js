f10d5d21034cc4c194eae3787223a2542d7cc89a
crystalodi <crystalodi@gmail.com>
2018-06-23 00:57:22 -0500

    add row, jumbotron, and container components

---
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Jumbotron = props => (
+    <div className="jumbotron mt-2">
+        <h1 className="display-1">NYT Search</h1>
+    </div>
+)
+
+export default Jumbotron;
