2b7325a836fe30e09a888041de20c8176f4cf347
crystalodi <crystalodi@gmail.com>
2018-06-24 11:10:59 -0500

    in response from nyt api, return title, url, and articleid

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,6 +4,17 @@ export default {
     articleSearch: (params) => {
         const apiKey = "297e0813c14744f492ff37e0c8db8a3e";
         const url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=${apiKey}&q=${params.q}`
-        return axios.get(url).then(response => response.data.response.docs)
+        return axios.get(url).then((response) => {
+            const respArticles = response.data.response.docs;
+            const articles = [];
+            respArticles.forEach(element => {
+                const node = {}
+                node["url"] = element.web_url;
+                node["title"] = element.headline.main;
+                node["articleID"] = element._id;
+                articles.push(node);
+            })
+            return articles;
+        })
     }
 }
