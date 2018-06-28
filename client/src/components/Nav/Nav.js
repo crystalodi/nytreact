7649fd9939ae549b1c8c0ea118d2af0c33b8252b
crystalodi <crystalodi@gmail.com>
2018-06-27 22:09:59 -0500

    replace class with className

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,14 +1,14 @@
 import React from "react";
 
 const Nav = () => (
-<nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="/">
+<nav className="navbar navbar-default">
+  <div className="container-fluid">
+    <div className="navbar-header">
+      <a className="navbar-brand" href="/">
         <span className="glyphicon glyphicon-home"></span>
       </a>
     </div>
-    <ul class="nav navbar-nav">
+    <ul className="nav navbar-nav">
       <li><a href="/">Home</a></li>
       <li><a href="/saved">Saved</a></li>
     </ul>
